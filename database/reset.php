354d672015d1b062c13653e82d9357f1eaafd26d
Max Krog <krog.max@gmail.com>
2015-11-30 18:38:43 +0100

    Error handling

--- database/reset.php
+++ database/reset.php
@@ -12,11 +12,23 @@ if (mysqli_connect_errno()) {
 // The SQL query
 $query = file_get_contents("reset.sql");
 
-// Execute the query
-$mysqli->multi_query($query);
 
-$result = $mysqli->fetch_all();
-print($result);
+// Execute multi query
+if ($mysqli->multi_query($query)) {
+    do {
+        // store first result set
+        if ($result = $mysqli->store_result()) {
+            while ($row = $result->fetch_row()) {
+                printf("%s\n", $row[0]);
+            }
+            $result->free();
+        }
+        // print divider
+        if ($mysqli->more_results()) {
+            printf("-----------------\n");
+        }
+    } while ($mysqli->next_result());
+}
 
 $mysqli->close(); //Close DB-Connection
 ?>
