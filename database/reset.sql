0dab747595d766b1ae5998be30ca7f46cd637e1e
Max Krog <krog.max@gmail.com>
2015-11-30 16:26:11 +0100

    Fixed SQL file for mysql

--- database/reset.sql
+++ database/reset.sql
@@ -1,38 +1,38 @@
-DROP TABLE IF EXISTS "users" CASCADE;
-DROP TABLE IF EXISTS "posts" CASCADE;
-DROP TABLE IF EXISTS "tags" CASCADE;
-DROP TABLE IF EXISTS "post_tags" CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
+DROP TABLE IF EXISTS tags CASCADE;
+DROP TABLE IF EXISTS post_tags CASCADE;
 
 
-CREATE TABLE "users" (
-	"username" text,
-	"password" text,
-	"role" text,
-	Constraint "users_pkey" Primary Key ("username")
+CREATE TABLE users (
+	username varchar(10),
+	password text,
+	role text,
+	Constraint users_pkey Primary Key (username)
 );
 
 
-CREATE TABLE "posts" (
-	"post_id" integer NOT NULL,
-	"image" text,
-	"title" text,
-	"caption" text,
-	"date" date,
-	"lat" double precision,
-	"lng" double precision,
-	"username" text references users(username),
-	Constraint "sports_pkey" Primary Key ("post_id")
+CREATE TABLE posts (
+	post_id integer NOT NULL,
+	image text,
+	title text,
+	caption text,
+	date date,
+	lat double precision,
+	lng double precision,
+	username varchar(10) references users(username),
+	Constraint sports_pkey Primary Key (post_id)
 );
 
-CREATE TABLE "tags" (
-	"tag" text,
-	Constraint "tags_pkey" Primary Key("tag")
+CREATE TABLE tags (
+	tag varchar(20),
+	Constraint tags_pkey Primary Key(tag)
 );
 
-CREATE TABLE "post_tags" (
-	"post_id" integer references posts(post_id),
-	"tag" text references tags(tag),
-	Constraint "post_tags_pkey" Primary Key("post_id", "tag")
+CREATE TABLE post_tags (
+	post_id integer references posts(post_id),
+	tag varchar(20) references tags(tag),
+	Constraint post_tags_pkey Primary Key(post_id, tag)
 );
 
 
