289bc0f68c3401d7e4f5a28532baa220584ebe24
MaxKrog <krog.max@gmail.com>
2016-01-12 02:10:05 +0100

    Added

--- pages/post.php
+++ pages/post.php
@@ -16,31 +16,38 @@ if( mysqli_num_rows($result) == 0){
 	echo("No post was found for that ID. You now have to write an angry note.");
 	die();
 }
-$line = $result->fetch_object();
+//HERE WE GOT THE MAIN POST-INFO
+$post = $result->fetch_object();
+mysqli_free_result($result);
 
+$query = "SELECT tag FROM post_tags WHERE post_id = '$post_id';";
+$result = mysqli_query($mysqli, $query);
+$tags = "";
+
+while($line = $result->fetch_object()){
+	$tags .= "<tag>" . $line->tag . "</tag>";
+}
 
 header("Content-type: text/xml; charset=utf-8"); ?>
 <?xml version="1.0" encoding="utf-8"?>
-
-
-<!DOCTYPE post SYSTEM "http://xml.csc.kth.se/~mkrog/DM2517/projekt/dtd/post.dtd">
+<!DOCTYPE kriz SYSTEM "http://xml.csc.kth.se/~mkrog/DM2517/projekt/dtd/post.dtd">
 <kriz>
 	<?php 
 	include("../misc/userinfo.php");
 	userinfo();
 	?>
-	<post>
-		<title><?php echo($line->title); ?> </title>
-		<caption><?php echo($line->caption); ?> </caption>
-		<imagemedium><?php echo($line->imagemedium); ?> </imagemedium>
-		<imagethumb><?php echo($line->imagethumb); ?> </imagethumb>
+	<post id='<?php echo($post_id) ?>'>
+		<title><?php echo($post->title); ?> </title>
+		<caption><?php echo($post->caption); ?> </caption>
+		<imagemedium><?php echo($post->image_medium); ?> </imagemedium>
+		<imagethumb><?php echo($post->image_thumb); ?> </imagethumb>
 		<location>
-			<lat><?php echo($line->lat); ?> </lat>
-			<lng><?php echo($line->lng); ?> </lng>
+			<lat><?php echo($post->lat); ?> </lat>
+			<lng><?php echo($post->lng); ?> </lng>
 		</location>
-		<user><?php echo($line->user); ?> </user>
-		<createdat><?php echo($line->createdat); ?> </createdat>
-		<tags><?php //echo($line->tags); ?> </tags>
+		<user><?php echo($post->username); ?> </user>
+		<createdat><?php echo($post->createdAt); ?> </createdat>
+		<tags><?php echo($tags); ?> </tags>
 
     </post>
 </kriz>
